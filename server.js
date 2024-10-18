9e4fb62aa8f42e67d2a54fa20d4db1a23faefb81
dantvi <daniel.tving@gmail.com>
2024-10-18 20:35:13 +0200

    register user

--- server.js
+++ server.js
@@ -3,8 +3,36 @@ const cors = require('cors');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('This is working');
+app.use(cors());
+app.use(express.json());
+
+const database = {
+  users: [
+    {
+      id: '1',
+      email: 'john@gmail.com',
+      joined: new Date()
+    },
+    {
+      id: '2',
+      email: 'ann@gmail.com',
+      joined: new Date()
+    }
+  ]
+}
+
+app.get('/users', (req, res) => {
+  res.send(database.users);
+});
+
+app.post('/register', (req, res) => {
+  const { email } = req.body;
+  database.users.push({
+    id: database.users.length + 1,
+    email: email,
+    joined: new Date()
+  });
+  res.json(database.users[database.users.length - 1]);
 });
 
 app.listen(3000, () => {
